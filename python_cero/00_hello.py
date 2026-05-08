bfa348350e8e6d1300eba570c9546b76449db498
unknown <jhonsdb>
2026-05-08 19:06:11 +0200

    actividades

---
+++ python_cero/00_hello.py
@@ -0,0 +1,11 @@
+#hola mundo 
+print("hello word")
+"""
+comentario en una sola linea 
+"""
+#consultar el tipo de dato
+print(type('soy un dato Str'))
+print(type(5))
+print(type(1.5))
+print(type(3 + 1))
+print(type(True))
---
+++ python_cero/01_variables.py
@@ -0,0 +1,12 @@
+#Variables
+
+my_string_variable = "My variable String"
+print(my_string_variable)
+
+my_init_variable = 5
+print(my_init_variable)
+
+my_bool_varial = False
+print(my_bool_varial) 
+
+print(my_string_variable , my_init_variable , my_bool_varial)
