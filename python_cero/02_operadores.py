56cafeadf7376591961d8b97a47790fd79c52d4b
jhonsdb <you@example.com>
2026-05-11 15:42:08 +0200

    operadores

---
+++ python_cero/02_operadores.py
@@ -0,0 +1,35 @@
+#OPERADORESS 
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(10 % 2)
+print(10 // 3)
+print(2 ** 3) #calcular con exponenete
+print(2 ** 3 + 3 -7 /1 // 4)
+
+print('hola ' + 'python')
+print('hola' + str(5))
+print('hola ' * 5)
+
+###Operadores comparattivos###
+
+print(3 > 4)
+print(3 < 4)
+print(3 <= 4)
+print(3 >= 4 )
+print(3 == 4)
+print(3 != 4)
+print('####')
+print('hola' > 'pyhon') #comprueba el numero de caracteres y deveulve el resultadp
+print('hola' < 'pyhon')
+print('aaaa' >= 'abaa') #ordenacion alfabetica
+print(len('aaaa') >= len('abaa')) #cuena alfabettica
+print('hola' <= 'pyhon')
+print('hola' == 'pyhon')
+print('hola' != 'pyhon')
+
+###OPERADORES LOGICOS###
+print(3 > 4 and 'Hola' > 'Python')
+print(3 > 4 or 'Hola' > 'Python')
+print(not(3>4))
