7b4c523e7631edb7b8fa7504bdc9231f01b83330
jhonsdb <you@example.com>
2026-05-13 21:42:43 +0200

    listas

---
+++ python_cero/04_listas.py
@@ -0,0 +1,22 @@
+#LISTAS/LISTS#\
+
+my_list = list()
+my_other_list = []
+
+
+
+
+my_list = [35,24,62,52,30,30,17]
+
+print(my_list)
+print(len(my_list))
+
+my_other_list  = [35,1.77, "jhon",'davila']
+
+print(type(my_other_list))
+print(my_other_list[0])
+print(my_other_list[1])
+print(my_other_list[-1])
+print(my_other_list[-3])
+#print(my_other_list[4]) IndexError
+#print(my_other_list[-4]) IndexError
